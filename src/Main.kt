67b7a5b5677e9530e93316c336891f0e973941f7
Xian Dev <xiandev25@gmail.com>
2025-11-16 20:50:44 +0100

    Create an instance of a class

--- src/Main.kt
+++ src/Main.kt
@@ -11,14 +11,10 @@ class SmartDevice {
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
+    // 3. Create an instance of a class
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    // val variables that contain an object reference are read-only themselves, but the objet proper they are
+    // referencing remains mutable.
+    // The following initializes a read-only variable smartTvDevice with an instance of the class SmartDevice.
+    val smartTvDevice = SmartDevice()
 }
