ce0d9099e121b0cde90cf5171d6dd89ba02dfa0c
Xian Dev <xiandev25@gmail.com>
2025-11-18 21:41:01 +0100

    Add a vis-modifier to a method

--- src/Main.kt
+++ src/Main.kt
@@ -89,7 +89,7 @@ class SmartTvDevice(deviceName: String, deviceCategory: String):
         println("Speaker volume increased to $speakerVolume")
     }
 
-    fun nextChannel() {
+    protected fun nextChannel() {
         channelNumber++
         println("Next channel increased to $channelNumber")
     }
