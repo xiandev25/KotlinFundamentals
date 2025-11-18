58e3e58585ec91bf36a2aba18e8e26a696f3652d
Xian Dev <xiandev25@gmail.com>
2025-11-18 20:36:58 +0100

    Add a proprety and a method to the new class

--- src/Main.kt
+++ src/Main.kt
@@ -95,7 +95,17 @@ class SmartTvDevice(deviceName: String, deviceCategory: String):
 class SmartLightDevice(deviceName: String, deviceCategory: String):
     SmartDevice(name = deviceName, category = deviceCategory) {
 
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
 
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel")
+    }
 }
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
